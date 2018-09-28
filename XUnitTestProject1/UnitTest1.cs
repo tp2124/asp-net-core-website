62908b9fd0022f9e502ff81c76cd75bfd4536e75
tp2124 <tp2124@gmail.com>
2018-09-27 22:30:53 -0700

    Adding XUnit .NETCore Testing

---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+            // This code will initialize prior to any tests being run.
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            //Student student1 = new Student() { Name = "Jenny" };
+            Assert.False(false);
+        }
+    }
+}
